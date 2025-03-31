e718458221abf12a5ad418092b8318c4ee7c5fa1
Rogerio <rogerio.cruz.arroyo@gmail.com>
2025-03-31 09:08:04 -0300

    declarações e variaveis

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+//DECLARAÇÕES E VARIAVEIS
+
+var nome ="fiap";
+console.log (nome);
+
+let idade = 19;
+console.log(idade);
+
+const sobrenome="Fiapinho";
+console.log(sobrenome);
+
+//declarando um variavel indefinida
+let nome;
+console.log(nome1)
+//declarando uma variavel nula
+let nome2=null
+console.log(nome2)
