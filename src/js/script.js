43b9e83804f5ef650829dac84e0b9fc39cc4ef52
Rogerio <rogerio.cruz.arroyo@gmail.com>
2025-03-31 09:29:11 -0300

    conhecendo alguns metodos

--- src/js/script.js
+++ src/js/script.js
@@ -12,12 +12,12 @@ console.log(sobrenome);
 //declarando um variavel indefinida
 let nome;
 console.log(nome1)
+
 //declarando uma variavel nula
 let nome2=null
 console.log(nome2)
 
 //TIPOS DE VARIAVEIS
-
 let exemplo1 = 18;
 console.log(typeof exemplo1)
 
@@ -34,11 +34,22 @@ let exemplo5 =[];
 console.log(typeof exemplo5)
 
 //CONVERSÕES
-
 let numFloat =123.456;
 console.log(parseInt(numFloat));
 
 let numString ="123.456"
 console.log(parseFloat(numString));
 
+//METODOS
 
+//lenght = verifica o tamanho da string
+let frase ="O mundo da tecnologia"
+console.log(frase.length); 
+
+//indexOF = retorna um trecho do texto (a posição)
+let texto="A programação indomável e sustentavel"
+console.log(texto.indexOf("ão"))
+
+//slice = retornar um trecho do texto com inicio e final
+let info="Processamento de ponto"
+console.log(info.slice(2,6));
